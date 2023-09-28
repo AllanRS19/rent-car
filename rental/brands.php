a4589e58b370f2a07bfa537dc68471fe9e7be416
allanrs19 <allanrs19@gmail.com>
2023-09-28 02:20:43 -0400

    implemented brands card screen

---
+++ rental/brands.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../assets/styles/global.css">
+    <link rel="stylesheet" href="../assets/styles/brands.css">
+    <title>Rental Car - Marcas</title>
+</head>
+
+<body>
+
+    <!-- <div class="overlay">
+        <div class="close-overlay-icon">
+            <i class='bx bx-x'></i>
+        </div>
+        <div class="add-fuel-form-container">
+
+            <div class="add-fuel-form-container-top">
+                <h2>Añadir Tipo de Combustible</h2>
+            </div>
+
+            <form action="" method="">
+
+                <div class="input-container">
+                    <label for="">Nombre de Combustible</label>
+                    <input type="text" class="fuel-name">
+                </div>
+
+                <div class="input-container">
+                    <label for="">Descripción de Combustible</label>
+                    <textarea name="" id="" cols="30" rows="5" class="fuel-description"></textarea>
+                </div>
+
+                <div class="input-container">
+                    <label for="">Estado de Combustible</label>
+                    <select name="" id="" class="fuel-status">
+                        <option value="None">Escoge una opción</option>
+                        <option value="Disponible">Disponible</option>
+                        <option value="Disponible">No Disponible</option>
+                    </select>
+                </div>
+
+                <button class="add-fuel-btn">
+                    Añadir Combustible
+                </button>
+
+            </form>
+        </div>
+    </div> -->
+    
+    <?php include "./nav-template.php"; ?>
+
+            <div class="title-wrapper">
+
+                <div class="title-left">
+                    <h1 class="title">Marcas</h1>
+                    <ul class="breadcrumbs">
+                        <li><a href="#">Inicio</a></li>
+                        <li class="divider">/</li>
+                        <li><a href="#" class="active">Marcas</a></li>
+                    </ul>
+                </div>
+
+                <div class="title-right">
+                    <button class="add-feature-btn brands-btn">
+                        <span class="add-feature-btn-icon">
+                            <i class='bx bx-plus'></i>
+                        </span>
+                        <span class="add-feature-btn-text">Añadir marca</span>
+                    </button>
+                </div>
+
+            </div>
+
+            <hr>
+
+            <div class="info-data brands">
+
+                <div class="card brands">
+                    <div class="card-content">
+                        <h4 class="card-content-title">Toyota</h4>
+                        <p class="card-content-description">Es una mezcla de hidrocarburos obtenida de la destilación fraccionada del petróleo kdklj aque losd mde aklsnfkl aopqmfmw</p>
+                        <div class="card-action-btns">
+                            <button class="edit-brand">Editar</button>
+                            <button class="delete-brand">Eliminar</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+        </main>
+        <!-- MAIN -->
+    </section>
+    <!-- NAVBAR -->
+
+    <script src="../assets/scripts/dashboard.js"></script>
+</body>
+
+</html>
